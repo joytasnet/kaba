88f256c1dc7d4e817914b687ab83783937f9266c
mjpurin <mjpurin@gmail.com>
2024-02-27 13:22:07 +0900

    bs2.py追加

---
+++ bs2.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://joytas.net/kaba/')
+res.encoding=res.apparent_encoding
+
+soup = BeautifulSoup(res.text,'html.parser')
+
+#print(soup)
+
+#タグで要素取得
+ele = soup.find('title')
+print(ele.text)
